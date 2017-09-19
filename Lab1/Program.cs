f4975e85f9c8c903a1012c9a0126f25a6ad414c6
Михаил Федин <vbif.3618@gmail.com>
2017-09-19 18:06:29 +0300

    fix1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
+using Newtonsoft.Json;
 
 namespace Lab1
 {
@@ -15,19 +17,19 @@ namespace Lab1
             //    @"http://www.neracoos.org/erddap/tabledap/E05_aanderaa_all.json?station%2Cmooring_site_desc%2Cwater_depth%2Ctime%2Ccurrent_speed%2Ccurrent_speed_qc%2Ccurrent_direction%2Ccurrent_direction_qc%2Ccurrent_u%2Ccurrent_u_qc%2Ccurrent_v%2Ccurrent_v_qc%2Ctemperature%2Ctemperature_qc%2Cconductivity%2Cconductivity_qc%2Csalinity%2Csalinity_qc%2Csigma_t%2Csigma_t_qc%2Ctime_created%2Ctime_modified%2Clongitude%2Clatitude%2Cdepth&time%3E=2015-08-25T15%3A00%3A00Z&time%3C=2016-12-05T14%3A00%3A00Z";
             //var wc = new WebClient();
             //var json = wc.DownloadString(link);
-            var path = @"Загрузки\E05_aanderaa_all_1769_d432_5004";
+            var path = @"C:\Users\нано\Downloads\E05_aanderaa_all_1769_d432_5004.json";
             var json = File.ReadAllText(path);
 
             var characteristics = new Dictionary<string, dynamic>
             {
-                {"start_date",DateTime.MaxValue},
-                {"end_date",DateTime.MinValue},
+                {"start_date",null},
+                {"end_date",null},
                 {"num_records",0},
                 {"min_COLUMN",float.MaxValue},
-                {"min_time",DateTime.MinValue},
+                {"min_time",null},
                 {"max_COLUMN",float.MinValue},
-                {"max_time",DateTime.MinValue},
-                {"avg_COLUMN",0},
+                {"max_time",null},
+                {"avg_COLUMN",0.00},
             };
             var parameters = new[] { "current_speed", "temperature", "salinity" };
             var columnNumber = new List<int>(parameters.Length);
@@ -42,18 +44,44 @@ namespace Lab1
                     columnNumber.Add(i);
 
             var result = new Dictionary<string, Dictionary<string, dynamic>>(parameters.Length);
-            foreach (string t in parameters)
-                result.Add(t, characteristics);
+            foreach (string param in parameters)
+                result.Add(param, characteristics);
 
             for (var i = 0; i < rows.Count(); i++)
-            for (var j = 0; j < columnNumber.Count; j++)
-                if (Math.Abs(rows[i].Value<float>(columnNumber[j] + 1)) < 0.1)
-                {
-                    var date = rows[i][dateColumn].Value<float>();
-                    var value = rows[i][columnNumber[j]].Value<float>();
-                }
+                for (var j = 0; j < columnNumber.Count; j++)
+                    if (Math.Abs(rows[i].Value<float>(columnNumber[j] + 1)) < 0.1)
+                    {
+                        var date = rows[i][dateColumn].Value<string>();
+                        var value = rows[i][columnNumber[j]].Value<float>();
+                        if (i == 0)
+                            result[parameters[j]]["start_date"] = date;
 
 
+                        if (result[parameters[j]]["min_COLUMN"] > value)
+                        {
+                            result[parameters[j]]["min_COLUMN"] = value;
+                            result[parameters[j]]["min_time"] = date;
+                        }
+                        if (result[parameters[j]]["max_COLUMN"] < value)
+                        {
+                            result[parameters[j]]["max_COLUMN"] = value;
+                            result[parameters[j]]["max_time"] = date;
+                        }
+
+                        result[parameters[j]]["num_records"]++;
+                        result[parameters[j]]["avg_COLUMN"] += value;
+
+                        if (i == rows.Count() - 1)
+                        {
+                            result[parameters[j]]["end_date"] = date;
+                            result[parameters[j]]["avg_COLUMN"] /= result[parameters[j]]["num_records"];
+                        }
+                    }
+            var resultJson = JsonConvert.SerializeObject(result);
+            resultJson = resultJson.Replace("{", "{\n");
+            resultJson = resultJson.Replace("}", "\n}");
+            resultJson = resultJson.Replace(",", ",\n");
+            Console.WriteLine(resultJson);
             Console.Read();
         }
 
