87a4e6dbb4bdbd67fb2bfafa69a34327c0b0dcc6
Михаил <Vbif.32@ya.ru>
2017-09-19 20:44:27 +0300

    смена источника

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -11,25 +12,25 @@ namespace Lab1
     {
         private static void Main(string[] args)
         {
-            //const string link =
-            //    @"http://www.neracoos.org/erddap/tabledap/E05_aanderaa_all.json?station%2Cmooring_site_desc%2Cwater_depth%2Ctime%2Ccurrent_speed%2Ccurrent_speed_qc%2Ccurrent_direction%2Ccurrent_direction_qc%2Ccurrent_u%2Ccurrent_u_qc%2Ccurrent_v%2Ccurrent_v_qc%2Ctemperature%2Ctemperature_qc%2Cconductivity%2Cconductivity_qc%2Csalinity%2Csalinity_qc%2Csigma_t%2Csigma_t_qc%2Ctime_created%2Ctime_modified%2Clongitude%2Clatitude%2Cdepth&time%3E=2015-08-25T15%3A00%3A00Z&time%3C=2016-12-05T14%3A00%3A00Z";
-            //var wc = new WebClient();
-            //var json = wc.DownloadString(link);
-            var path = @"C:\Users\Vbif3\Downloads\E05_aanderaa_all_1769_d432_5004.json";
-            var json = File.ReadAllText(path);
+            const string link =
+                @"http://www.neracoos.org/erddap/tabledap/E05_aanderaa_all.json?station%2Cmooring_site_desc%2Cwater_depth%2Ctime%2Ccurrent_speed%2Ccurrent_speed_qc%2Ccurrent_direction%2Ccurrent_direction_qc%2Ccurrent_u%2Ccurrent_u_qc%2Ccurrent_v%2Ccurrent_v_qc%2Ctemperature%2Ctemperature_qc%2Cconductivity%2Cconductivity_qc%2Csalinity%2Csalinity_qc%2Csigma_t%2Csigma_t_qc%2Ctime_created%2Ctime_modified%2Clongitude%2Clatitude%2Cdepth&time%3E=2015-08-25T15%3A00%3A00Z&time%3C=2016-12-05T14%3A00%3A00Z";
+            var wc = new WebClient();
+            var json = wc.DownloadString(link);
+            //var path = @"C:\Users\Vbif3\Downloads\E05_aanderaa_all_1769_d432_5004.json";
+            //var json = File.ReadAllText(path);
 
             var characteristics = new Dictionary<string, dynamic>
             {
-                {"start_date",null},
-                {"end_date",null},
-                {"num_records",0},
-                {"min_COLUMN",float.MaxValue},
-                {"min_time",null},
-                {"max_COLUMN",float.MinValue},
-                {"max_time",null},
-                {"avg_COLUMN",0.00}
+                {"start_date", null},
+                {"end_date", null},
+                {"num_records", 0},
+                {"min_COLUMN", float.MaxValue},
+                {"min_time", null},
+                {"max_COLUMN", float.MinValue},
+                {"max_time", null},
+                {"avg_COLUMN", 0.00}
             };
-            var parameters = new[] { "current_speed", "temperature", "salinity" };
+            var parameters = new[] {"current_speed", "temperature", "salinity"};
             var columnNumber = new List<int>(parameters.Length);
             var dateColumn = 3;
 
@@ -46,45 +47,45 @@ namespace Lab1
             {
                 result.Add(param, new Dictionary<string, dynamic>
                 {
-                    {"start_date",null},
-                    {"end_date",null},
-                    {"num_records",0},
-                    {"min_COLUMN",float.MaxValue},
-                    {"min_time",null},
-                    {"max_COLUMN",float.MinValue},
-                    {"max_time",null},
-                    {"avg_COLUMN",0.00}
+                    {"start_date", null},
+                    {"end_date", null},
+                    {"num_records", 0},
+                    {"min_COLUMN", float.MaxValue},
+                    {"min_time", null},
+                    {"max_COLUMN", float.MinValue},
+                    {"max_time", null},
+                    {"avg_COLUMN", 0.00}
                 });
             }
-                
+
 
             for (var i = 0; i < rows.Count(); i++)
-                for (var j = 0; j < columnNumber.Count; j++)
-                    if (rows[i].Value<float>(columnNumber[j] + 1) == 0)
+            for (var j = 0; j < columnNumber.Count; j++)
+                if (rows[i].Value<float>(columnNumber[j] + 1) == 0)
+                {
+                    var date = rows[i][dateColumn].Value<DateTime>();
+                    var value = rows[i][columnNumber[j]].Value<float>();
+                    if (i == 0)
+                        result[parameters[j]]["start_date"] = date;
+
+
+                    if (result[parameters[j]]["min_COLUMN"] > value)
                     {
-                        var date = rows[i][dateColumn].Value<DateTime>();
-                        var value = rows[i][columnNumber[j]].Value<float>();
-                        if (i == 0)
-                            result[parameters[j]]["start_date"] = date;
-
-
-                        if (result[parameters[j]]["min_COLUMN"] > value)
-                        {
-                            result[parameters[j]]["min_COLUMN"] = value;
-                            result[parameters[j]]["min_time"] = date;
-                        }
-                        if (result[parameters[j]]["max_COLUMN"] < value)
-                        {
-                            result[parameters[j]]["max_COLUMN"] = value;
-                            result[parameters[j]]["max_time"] = date;
-                        }
-
-                        result[parameters[j]]["num_records"]++;
-                        result[parameters[j]]["avg_COLUMN"] += value;
-                        result[parameters[j]]["end_date"] = date;
+                        result[parameters[j]]["min_COLUMN"] = value;
+                        result[parameters[j]]["min_time"] = date;
+                    }
+                    if (result[parameters[j]]["max_COLUMN"] < value)
+                    {
+                        result[parameters[j]]["max_COLUMN"] = value;
+                        result[parameters[j]]["max_time"] = date;
                     }
 
-            foreach(var param in parameters)
+                    result[parameters[j]]["num_records"]++;
+                    result[parameters[j]]["avg_COLUMN"] += value;
+                    result[parameters[j]]["end_date"] = date;
+                }
+
+            foreach (var param in parameters)
                 result[param]["avg_COLUMN"] /= result[param]["num_records"];
 
             var resultJson = JsonConvert.SerializeObject(result);
